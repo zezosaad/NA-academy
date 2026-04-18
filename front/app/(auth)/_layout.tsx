b84fc61447f1c44595ac0785975cb51a8d4fdc8b
ahmed-abdelmottal3 <abdelmottale3@gmail.com>
2026-04-17 23:34:54 -0700

    add basic packages

---
+++ front/app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+export default function AuthLayout() {
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="login" />
+            <Stack.Screen name="register" />
+        </Stack>
+    )
+}
